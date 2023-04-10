f13905f52a23a7cea6939e42c1b41467a40568e4
t120297346 <b05611018@ntu,edu.tw>
2023-04-10 23:07:39 +0800

    frontend user api

---
+++ frontend/src/services/axiosClient.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "/api/v1",
+});
+export default api;
---
+++ frontend/src/services/index.js
@@ -0,0 +1,7 @@
+import { user } from "./user";
+
+const services = {
+    user,
+};
+
+export default services
